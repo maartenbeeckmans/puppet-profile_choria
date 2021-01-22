247fac8337f9dda5ff0a018ebf67b0efbd38df4a
Maarten Beeckmans <maartenb@inuits.eu>
2021-01-22 21:49:45 +0100

    initial config

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,25 @@
 #
-class profile_template (
+#
+#
+class profile_choria (
+  Boolean $client,
+  Boolean $manage_firewall_entry,
 ) {
+  if $client {
+    class { 'choria':
+      manage_package_repo => true,
+    }
+  } else {
+    class { 'choria::broker':
+      network_broker => true,
+    }
+  }
+
+  if $manage_firewall_entry {
+    firewall { '04223 accept choria':
+      dport  => '4223',
+      proto  => 'tcp',
+      action => 'accept',
+    }
+  }
 }
