5164036e818fc7d275dde1176a71047461652aee
Maarten Beeckmans <maartenb@inuits.eu>
2021-01-23 21:52:32 +0100

    use correct terminology

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,22 +2,29 @@
 #
 #
 class profile_choria (
-  Boolean $server,
+  Boolean $broker,
   Boolean $manage_firewall_entry,
 ) {
-  if $server {
+  class { 'choria':
+    manage_package_repo => true,
+  }
+
+  if $broker {
     class { 'choria::broker':
       network_broker => true,
     }
-  } else {
-    class { 'choria':
-      manage_package_repo => true,
+    if $manage_firewall_entry {
+      firewall { '04223 accept choria peers':
+        dport  => '4223',
+        proto  => 'tcp',
+        action => 'accept',
+      }
     }
   }
 
   if $manage_firewall_entry {
-    firewall { '04223 accept choria':
-      dport  => '4223',
+    firewall { '04222 accept choria':
+      dport  => '4222',
       proto  => 'tcp',
       action => 'accept',
     }
